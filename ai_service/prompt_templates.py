d5f53b783415963d53a28cf8255ba079ea597ed0
Prashanth <prashanth.bmj16@gmail.com>
2025-12-18 17:30:42 +0530

    Update prompt_templates.py

--- ai_service/prompt_templates.py
+++ ai_service/prompt_templates.py
@@ -1,1 +1,12 @@
+RAG_PROMPT = """
+You are an enterprise eCommerce assistant.
+Answer ONLY from the provided context.
+If data is missing, say "Information not available".
+
+Context:
+{context}
+
+Question:
+{question}
+"""
 
