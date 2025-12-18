1c9839bcb351d76e8d53e5b9efe7bac6fa4a0ee2
Prashanth <prashanth.bmj16@gmail.com>
2025-12-18 19:42:16 +0530

    Update test_intent_classifier.py

--- tests/test_intent_classifier.py
+++ tests/test_intent_classifier.py
@@ -1,1 +1,7 @@
+from agent_service.agent_graph import build_agent_graph
+
+def test_intent_detection():
+    graph = build_agent_graph()
+    out = graph.invoke({"user_query": "please cancel my order ORD2911"})
+    assert out["result"]
 
