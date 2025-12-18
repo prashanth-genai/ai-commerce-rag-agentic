bd24417fb0c6e747c7545acb06b9a46274b1253b
Prashanth <prashanth.bmj16@gmail.com>
2025-12-18 17:45:44 +0530

    Update pricing_agent.py

--- agent_service/agents/pricing_agent.py
+++ agent_service/agents/pricing_agent.py
@@ -1,1 +1,14 @@
+def calculate_price(customer_tier, base_price, quantity):
+    discount = 0
+    if customer_tier == "GOLD":
+        discount = 0.15
+    elif customer_tier == "SILVER":
+        discount = 0.10
+
+    final_price = base_price * quantity * (1 - discount)
+    return {
+        "tier": customer_tier,
+        "discount": discount,
+        "final_price": final_price
+    }
 
