5983e4dd12bfc43e5c6bc6510ae8cd00d404ad27
Prashanth <prashanth.bmj16@gmail.com>
2025-12-18 17:43:38 +0530

    Update OrderController.java

--- java-commerce-mock/order-service/OrderController.java
+++ java-commerce-mock/order-service/OrderController.java
@@ -1,1 +1,14 @@
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    @GetMapping("/{userId}")
+    public Map<String, Object> getOrder(@PathVariable String userId) {
+        return Map.of(
+            "orderId", "ORD12345",
+            "status", "SHIPPED",
+            "deliveryDate", "2025-10-25"
+        );
+    }
+}
 
